e81807d1cc42c22607dfd445e2b36d5c76873e19
Calvin Tam <calvintam236@users.noreply.github.com>
2017-10-19 18:32:23 -0700

    Change to notification for AJAX error

--- assets/js/miningcore-ui.js
+++ assets/js/miningcore-ui.js
@@ -33,11 +33,20 @@ function loadPools() {
             if (poolList.length > 0) {
                 $('#poolList').append(poolList);
             }
+        })
+        .fail(function () {
+            $.notify({
+                icon: "ti-cloud-down",
+                message: "Error: No response from API. (loadPools)"
+            }, {
+                type: 'danger',
+                timer: 3000
+            });
         });
 }
 
 function loadStatsData() {
-    $.ajax(API + 'pools')
+    return $.ajax(API + 'pools')
         .always(function (data) {//debug,should be done()
             data = JSON.parse('{"pools":[{"id":"xmr1","coin":{"type":"XMR"},"ports":{"4032":{"difficulty":1600,"varDiff":{"minDiff":1600,"maxDiff":160000,"targetTime":15,"retargetTime":90,"variancePercent":30}},"4256":{"difficulty":5000}},"paymentProcessing":{"enabled":true,"minimumPayment":0.01,"payoutScheme":"PPLNS","payoutSchemeConfig":{"factor":2},"minimumPaymentToPaymentId":5},"banning":{"enabled":true,"checkThreshold":50,"invalidPercent":50,"time":600},"clientConnectionTimeout":600,"jobRebroadcastTimeout":55,"blockRefreshInterval":1000,"poolFeePercent":0,"donationsPercent":0,"poolStats":{"connectedMiners":0,"poolHashRate":0,"sharesPerSecond":0,"validSharesPerMinute":0,"invalidSharesPerMinute":0},"networkStats":{"networkType":"Test","networkHashRate":39.05,"networkDifficulty":2343,"lastNetworkBlockTime":"2017-09-17T10:35:55.0394813Z","blockHeight":157,"connectedPeers":2,"rewardType":"POW"}}]}');//debug
             //.done(function( data )) {
@@ -54,11 +63,20 @@ function loadStatsData() {
                     $('#networkDifficulty').text(value.networkStats.networkDifficulty);
                 }
             });
+        })
+        .fail(function () {
+            $.notify({
+                icon: "ti-cloud-down",
+                message: "Error: No response from API. (loadStatsData)"
+            }, {
+                type: 'danger',
+                timer: 3000
+            });
         });
 }
 
 function loadStatsChart() {
-    $.ajax(API + 'pool/' + currentPool + '/stats/hourly')
+    return $.ajax(API + 'pool/' + currentPool + '/stats/hourly')
         .always(function (data) {//debug,should be done()
             data = JSON.parse('{"stats":[{"poolHashRate":20,"connectedMiners":12,"created":"2017-09-16T10:00:00"},{"poolHashRate":25,"connectedMiners":15,"created":"2017-09-16T11:00:00"},{"poolHashRate":23,"connectedMiners":13,"created":"2017-09-17T10:00:00"}]}');//debug
             //.done(function( data )) {
@@ -149,19 +167,38 @@ function loadStatsChart() {
             ];
 
             Chartist.Line('#chartStatsMiners', data, options, responsiveOptions);
+        })
+        .fail(function () {
+            $.notify({
+                icon: "ti-cloud-down",
+                message: "Error: No response from API. (loadStatsChart)"
+            }, {
+                type: 'danger',
+                timer: 3000
+            });
         });
 
 };
 
 function loadDashboardData(walletAddress, workerName) {
-    $.ajax(API + 'pool/' + currentPool + '/miner/' + walletAddress + '/stats')
+    return $.ajax(API + 'pool/' + currentPool + '/miner/' + walletAddress + '/stats')
         .always(function (data) {//debug,should be done()
             data = JSON.parse('{"pendingShares":354,"pendingBalance":1.456,"totalPaid":12.354}');//debug
             //.done(function( data )) {
             //data = JSON.parse(data);
-            $('#unconfirmedBalance').text(data.pendingBalance);
             $('#unconfirmedShares').text(data.pendingShares);
-            $('#totalPaid').text(data.totalPaid);
+            //$('#minerHashRate').text(data.minerHashRate);
+            $('#unconfirmedBalance').text(data.pendingBalance);
+            //$('#lifetimeBalance').text(data.lifetimeBalance);
+        })
+        .fail(function () {
+            $.notify({
+                icon: "ti-cloud-down",
+                message: "Error: No response from API. (loadDashboardData)"
+            }, {
+                type: 'danger',
+                timer: 3000
+            });
         });
 }
 
@@ -170,7 +207,7 @@ function loadDashboardWorkerList(walletAddress, workerName) {
 }
 
 function loadDashboardChart(walletAddress, workerName) {
-    $.ajax(API + 'pool/' + currentPool + '/miner/' + walletAddress + '/stats')
+    return $.ajax(API + 'pool/' + currentPool + '/miner/' + walletAddress + '/stats')
         .always(function (data) {//debug,should be done()
             data = JSON.parse('{"stats":[{"poolHashRate":20,"connectedMiners":12,"created":"2017-09-16T10:00:00"},{"poolHashRate":25,"connectedMiners":15,"created":"2017-09-16T11:00:00"},{"poolHashRate":23,"connectedMiners":13,"created":"2017-09-17T10:00:00"}]}');//debug
             //.done(function( data )) {
@@ -230,11 +267,20 @@ function loadDashboardChart(walletAddress, workerName) {
             ];
 
             Chartist.Line('#chartDashboardHashRate', data, options, responsiveOptions);
+        })
+        .fail(function () {
+            $.notify({
+                icon: "ti-cloud-down",
+                message: "Error: No response from API. (loadDashboardChart)"
+            }, {
+                type: 'danger',
+                timer: 3000
+            });
         });
 }
 
 function loadBlocksList() {
-    $.ajax(API + 'pool/' + currentPool + '/blocks')
+    return $.ajax(API + 'pool/' + currentPool + '/blocks')
         .always(function (data) {//debug,should be done()
             data = JSON.parse('[{"blockHeight":2000197,"status":"pending","transactionConfirmationData":"6e7f68c7891e0f2fdbfd0086d88be3b0d57f1d8f4e1cb78ddc509506e312d94d","reward":17.55888124174,"infoLink":"https://xmrchain.net/block/6e7f68c7891e0f2fdbfd0086d88be3b0d57f1d8f4e1cb78ddc509506e312d94d","created":"2017-09-16T07:41:50.242856"},{"blockHeight":2000196,"status":"confirmed","transactionConfirmationData":"bb0b42b4936cfa210da7308938ad6d2d34c5339d45b61c750c1e0be2475ec039","reward":17.558898015821,"infoLink":"https://xmrchain.net/block/bb0b42b4936cfa210da7308938ad6d2d34c5339d45b61c750c1e0be2475ec039","created":"2017-09-16T07:41:39.664172"},{"blockHeight":2000195,"status":"orphaned","transactionConfirmationData":"b9b5943b2646ebfd19311da8031c66b164ace54a7f74ff82556213d9b54daaeb","reward":17.558914789917,"infoLink":"https://xmrchain.net/block/b9b5943b2646ebfd19311da8031c66b164ace54a7f74ff82556213d9b54daaeb","created":"2017-09-16T07:41:14.457664"}]');//debug
             //.done(function( data )) {
@@ -254,11 +300,20 @@ function loadBlocksList() {
             }
             blockList += '</tbody>';
             $('#blockList').html(blockList);
+        })
+        .fail(function () {
+            $.notify({
+                icon: "ti-cloud-down",
+                message: "Error: No response from API. (loadBlocksList)"
+            }, {
+                type: 'danger',
+                timer: 3000
+            });
         });
 }
 
 function loadPaymentsList() {
-    $.ajax(API + 'pool/' + currentPool + '/payments')
+    return $.ajax(API + 'pool/' + currentPool + '/payments')
         .always(function (data) {//debug,should be done()
             data = JSON.parse('[{"coin":"XMR","address":"9wviCeWe2D8XS82k2ovp5EUYLzBt9pYNW2LXUFsZiv8S3Mt21FZ5qQaAroko1enzw3eGr9qC7X1D7Geoo2RrAotYPwq9Gm8","amount":7.5354,"transactionConfirmationData":"9e7f68c7891e0f2fdbfd0086d88be3b0d57f1d8f4e1cb78ddc509506e312d94d","infoLink":"https://xmrchain.net/tx/9e7f68c7891e0f2fdbfd0086d88be3b0d57f1d8f4e1cb78ddc509506e312d94d","created":"2017-09-16T07:41:50.242856"}]');//debug
             //.done(function( data )) {
@@ -278,5 +333,14 @@ function loadPaymentsList() {
             }
             paymentList += '</tbody>';
             $('#paymentList').html(paymentList);
+        })
+        .fail(function () {
+            $.notify({
+                icon: "ti-cloud-down",
+                message: "Error: No response from API. (loadPaymentsList)"
+            }, {
+                type: 'danger',
+                timer: 3000
+            });
         });
 }
